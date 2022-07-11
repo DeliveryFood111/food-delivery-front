dccdd6ff27e52b6ea6c43fec61db133c39fee1b9
Vandamchik <cherepcock@gmail.com>
2022-07-11 18:03:27 +0300

    fixed class names, and fixed adaptive

--- src/components/PopularProducts.vue
+++ src/components/PopularProducts.vue
@@ -1,76 +1,76 @@
 <template>
-  <section class="popular_product">
-    <div class="popular_product__case_content">
-      <h3 class="popular_product__h3">
+  <section class="popular-product">
+    <div class="popular-product__case_content">
+      <h3 class="popular-product__title">
         MENU
       </h3>
-      <p class="popular_product__case_content-title">
+      <p class="popular-product__case_content-title">
         Popular Dishes
       </p>
-      <p class="popular_product__case_content-text">
+      <p class="popular-product__case_content-text">
         There is a game between the waiters in restaurant to see who serves the food to each table fastest. That led to attempting the Guinness Record.
       </p>
     </div>
-    <div class="popular_product__case_menu">
-      <div class="popular_product__case_menu_cells" >
-        <img src="../assets/ukraine-borsch.webp" class="popular_product__case_menu_cells_img">
-        <div class="popular_product__case_menu_cells_price">
-          <p class="popular_product__case_menu_cells_price-text">
+    <div class="popular-product__case_menu">
+      <div class="popular-product__case_menu_cells" >
+        <img src="../assets/ukraine-borsch.webp" class="popular-product__case_menu_cells_img">
+        <div class="popular-product__case_menu_cells_price">
+          <p class="popular-product__case_menu_cells_price-text">
             Chicken Manjoori
           </p>
-          <p class="popular_product__case_menu_cells_price-text">
+          <p class="popular-product__case_menu_cells_price-text">
             $15
           </p>
         </div>
-        <div class="popular_product__case_menu_cells_description">
+        <div class="popular-product__case_menu_cells_description">
           Dish relished by all age groups as a starter dish at parties.
         </div>
       </div>
-      <div class="popular_product__case_menu_cells" >
-        <img  src="../assets/vareniki.webp" class="popular_product__case_menu_cells_img">
-        <div class="popular_product__case_menu_cells_price">
-          <p class="popular_product__case_menu_cells_price-text">
+      <div class="popular-product__case_menu_cells" >
+        <img  src="../assets/vareniki.webp" class="popular-product__case_menu_cells_img">
+        <div class="popular-product__case_menu_cells_price">
+          <p class="popular-product__case_menu_cells_price-text">
             Hotdog
           </p>
-          <p class="popular_product__case_menu_cells_price-text">
+          <p class="popular-product__case_menu_cells_price-text">
             $10
           </p>
         </div>
-        <div class="popular_product__case_menu_cells_description">
+        <div class="popular-product__case_menu_cells_description">
           Grilled sausage served in the slit of a partially sliced bun.
         </div>
       </div>
-      <div class="popular_product__case_menu_cells" >
-        <img  src="../assets/salad.webp" class="popular_product__case_menu_cells_img">
-        <div class="popular_product__case_menu_cells_price">
-          <p class="popular_product__case_menu_cells_price-text">
+      <div class="popular-product__case_menu_cells" >
+        <img  src="../assets/salad.webp" class="popular-product__case_menu_cells_img">
+        <div class="popular-product__case_menu_cells_price">
+          <p class="popular-product__case_menu_cells_price-text">
             Fresh Salmon
           </p>
-          <p class="popular_product__case_menu_cells_price-text">
+          <p class="popular-product__case_menu_cells_price-text">
             $5
           </p>
         </div>
-        <div class="popular_product__case_menu_cells_description">
+        <div class="popular-product__case_menu_cells_description">
           Beat the health blues with our Super Immune Blue Juice Recipe.
         </div>
       </div>
-      <div class="popular_product__case_menu_cells" >
-        <img src="../assets/hachapuri-po-adjarski.webp" class="popular_product__case_menu_cells_img">
-        <div class="popular_product__case_menu_cells_price">
-          <p class="popular_product__case_menu_cells_price-text">
+      <div class="popular-product__case_menu_cells" >
+        <img src="../assets/hachapuri-po-adjarski.webp" class="popular-product__case_menu_cells_img">
+        <div class="popular-product__case_menu_cells_price">
+          <p class="popular-product__case_menu_cells_price-text">
             Veg Burger
           </p>
-          <p class="popular_product__case_menu_cells_price-text">
+          <p class="popular-product__case_menu_cells_price-text">
             $10
           </p>
         </div>
-        <div class="popular_product__case_menu_cells_description">
+        <div class="popular-product__case_menu_cells_description">
           Burgers may be made from ingredients like beans.
         </div>
       </div>
     </div>
-    <div class="popular_product__case_btn">
-      <button class="popular_product__case_btn-button"> See all dishes </button>
+    <div class="popular-product__case_btn">
+      <button class="popular-product__case_btn-button"> See all dishes </button>
     </div>
   </section>
 </template>
@@ -80,10 +80,9 @@ export default {
   name: "PopularProducts" ,
 }
 </script>
-
 <style scoped lang="scss">
 
-.popular_product {
+.popular-product {
   color: rgba(41, 46, 54, 1);
   background-color: rgba(220, 220, 220, 1);
   display: flex;
@@ -91,7 +90,7 @@ export default {
   text-align: center;
   padding: 40px;
 
-  &__h3 {
+  &__title {
     border-top: 2px solid rgba(225, 177, 104, 1);
     border-bottom: 2px solid rgba(225, 177, 104, 1);
     font-size: 15px;
@@ -129,13 +128,9 @@ export default {
     margin-bottom: 30px;
     flex-wrap: wrap;
 
-    @media screen and (max-width: 780px){
+    @media screen and (max-width: 900px){
       flex-direction: column;
       align-content: center;
-    }
-    @media screen and (max-width: 550px){
-      flex-direction: column;
-      align-items: center;
     }
     &_cells {
       display: flex;
@@ -147,12 +142,12 @@ export default {
         margin-left: 50px;
         width: 350px;
       }
+      @media screen and (max-width: 900px){
+        margin-left: unset;
+      }
 
       @media screen and (max-width: 780px){
-        margin-left: unset;
         align-items: center;
-      }
-      @media screen and (max-width: 550px){
         width: 280px;
       }
       &_img {
@@ -164,10 +159,6 @@ export default {
           width: 350px;
         }
         @media screen and (max-width: 780px){
-          width: 280px;
-          height: 230px;
-        }
-        @media screen and (max-width: 550px){
           width: 280px;
           height: 230px;
         }
@@ -193,7 +184,7 @@ export default {
           margin-bottom: 10px;
           width: 350px;
         }
-        @media screen and (max-width: 550px){
+        @media screen and (max-width: 780px){
           width: 280px;
         }
         &-text {
